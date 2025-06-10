da596f752707170c318d7222b51c14573df94b8c
Louise Anokye <144999655+LouA28@users.noreply.github.com>
2025-06-10 15:40:30 +0100

    Update r_installsTT.R

--- r_installsTT.R
+++ r_installsTT.R
@@ -6,14 +6,26 @@ if (!"Rcpp" %in% rownames(installed.packages())) {
   install.packages("Rcpp", repos = "https://cloud.r-project.org")
 }
 
+# Install spatial dependencies first
+spatial_packages <- c("sp", "sf", "terra", "raster")
+to_install_spatial <- setdiff(spatial_packages, rownames(installed.packages()))
+if (length(to_install_spatial)) {
+  install.packages(to_install_spatial, repos = "https://cloud.r-project.org")
+}
+
+# Check and install leaflet afterwards
+if (!"leaflet" %in% rownames(installed.packages())) {
+  install.packages("leaflet", repos = "https://cloud.r-project.org")
+}
+
 # Step 2: Define list of packages to install (excluding Rcpp)
 packages <- c(
   "arrow", "BH", "BiocManager", "brms", "cli", "cowplot", "crosstalk", "data.table", "devtools",
   "dplyr", "DT", "formattable", "gamm4", "gdalBindings", "ggnewscale", "ggplot2", "ggrepel",
   "glmmTMB", "gratia", "GT", "here", "htmltools", "htmlwidgets", "janitor", "kableExtra",
-  "lattice", "lazyeval", "leaflet", "lubridate", "mgcv", "nanoparquet", "nimble", "odbc",
+  "lattice", "lazyeval", "lubridate", "mgcv", "nanoparquet", "nimble", "odbc",
   "openxlsx", "packrat", "plotly", "png", "profvis", "purrr", "qgam", "raster", "renv",
-  "readr", "readxl", "reticulate", "rlang", "RPostgres", "rsconnect", "rstudioapi", "scales", "sf",
+  "readr", "readxl", "reticulate", "rlang", "RPostgres", "rsconnect", "rstudioapi", "scales",
   "shiny", "shinyBS", "shinycssloaders", "shinydashboard", "shinyjs", "slickR", "snakecase",
   "srvyr", "stringr", "svglite", "tidyr", "tidyverse", "tictoc", "tmap", "treemap", "treemapify",
   "viridis", "visNetwork", "writexl", "zoo", "corrplot", "DHARMa", "gclus", "GGally", "gstat",
